ec4c1698f982332187f708d48326556b92e6ff05
gua12345 <121714613+gua12345@users.noreply.github.com>
2024-06-24 23:34:55 +0800

    Update lpm-levels.c

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -122,8 +122,8 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 static bool print_parsed_dt;
 module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 
-static bool sleep_disabled_dev;
-module_param_named(sleep_disabled_dev, sleep_disabled_dev, bool, 0664);
+static bool sleep_disabled;
+module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
 
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
@@ -680,7 +680,7 @@ static inline bool lpm_disallowed(s64 sleep_us, int cpu, struct lpm_cpu *pm_cpu)
 	if (cpu_isolated(cpu))
 		goto out;
 
-	if (sleep_disabled_dev)
+	if (sleep_disabled)
 		return true;
 
 	bias_time = sched_lpm_disallowed_time(cpu);
